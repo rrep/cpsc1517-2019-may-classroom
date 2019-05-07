990e66fbefb9efa285d387b91be46c6813a91f0e
Paul D <pwduerr@gmail.com>
2019-05-06 18:45:52 -0600

    changes

---
+++ OOPsSolution/OOPsReview/MyData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+
+    //the default access permission is private
+    //if an outside user of this class wanted access to the class contents, then the access permissions need to be public.
+    //if the class was static, the class would not hold data. It would just be a set of methods that do not change.
+    //for a class to hold data, do not make it static, unless you want everyone to be able to access it (rw)
+    public class myData
+    {
+        public int height;
+        public int width;
+
+        
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public void SetHeight(int myDataHeight)
+        {
+            height = myDataHeight;
+        }
+       
+    }
+}
--- OOPsSolution/OOPsReview/Program.cs
+++ OOPsSolution/OOPsReview/Program.cs
@@ -10,7 +10,31 @@ namespace OOPsReview
     {
         static void Main(string[] args)
         {
+            /*
+            console is a reference to a system static class
+            static classes are used by typing the class name
+            static classes do not store variable data
+            static classes are used by everyone (so it is not private)
+            methods of a static class will be placed after the object 'dot' operator
+             */
             Console.WriteLine("Hello World");
+
+            //create an instance of a non static class
+            //if you create a declarative using just the class name then this is only a pointer that can hold an instance of the class
+            myData aVariable = null;
+            //current value of aVariable is null. It is just a pointer.
+            myData twoVariable = new myData();
+            //assign some height yo
+            twoVariable.SetHeight(12);
+            if (aVariable == null)
+            { 
+                Console.WriteLine("aVariable Doesn't got nothin in it.");
+            }
+
+            if (twoVariable == null)
+            {
+                Console.WriteLine("twoVariable Doesn't got nothin in it.");
+            }
         }
     }
 }
